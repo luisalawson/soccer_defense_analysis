f8aa2d882bfa0c046185e6d09ba2a738d14d2999
Luisa Lawson <luisalawson14@gmail.com>
2024-12-09 10:43:52 -0300

    test

--- .github/workflows/scripts/endpoint-check/endpoint-check.js
+++ .github/workflows/scripts/endpoint-check/endpoint-check.js
@@ -14,7 +14,7 @@ if (!GITHUB_TOKEN || !REPO || !OWNER || !PR_NUMBER) {
 } 
 
 function searchInternalKeyword(changedFiles) {
-    let internalEndpoints = new Map();
+    let internalEndpoints = [];
     changedFiles.forEach(filePath => {
         try {
             const absolutePath = path.resolve(filePath); 
@@ -23,15 +23,20 @@ function searchInternalKeyword(changedFiles) {
             const combinedExp = /'\/internal\/[^']+'|'internal\/[^']+'/g;
             const endpointsFound = content.match(combinedExp);
             if (endpointsFound) {
-                internalEndpoints.set(filePath, endpointsFound);
+                const uniqueEndpoints = new Set(endpointsFound);
+                uniqueEndpoints.forEach(endpoint => {
+                    internalEndpoints.push([filePath, endpoint]);
+                });
             }
         } catch (error) {
             console.error(`Error reading file ${filePath}:`, error);
         }
     });
 
-    return Array.from(internalEndpoints);
+    return internalEndpoints;
 }
+
+
 
 async function postComment(endpoints) {
     const octokit = new Octokit({ auth: GITHUB_TOKEN });
@@ -42,6 +47,7 @@ async function postComment(endpoints) {
             commentBody += `  - ${endpoint}\n`;
         });
     });
+    commentBody += `\nPlease follow the steps on this document: https://docs.google.com/document/d/1AMwAvWqhR-6HYIF32iB2Ecjv3IfqqoQSXX1ObzcOk8E/edit?usp=sharing `;
     await octokit.request('POST /repos/{owner}/{repo}/issues/{issue_number}/comments', {
         owner: OWNER,
         repo: REPO,
@@ -50,21 +56,15 @@ async function postComment(endpoints) {
         headers: { 'X-GitHub-Api-Version': '2022-11-28' }
     });
 }
+
 async function main() {
     try {
         const internalEndpoints = searchInternalKeyword(FILES);
         if (internalEndpoints.length > 0) {
             await postComment(internalEndpoints);
-            // let commentBody = `Please explain why you are using the following internal endpoints:\n`;
-            // internalEndpoints.forEach(([filePath, endpointList]) => {
-            //     commentBody += `- ${filePath}:\n`;
-            //     endpointList.forEach(endpoint => {
-            //         commentBody += `  - ${endpoint}\n`;
-            //     });
-            // });
             process.exit(1);
         } else {
-            console.log("No internal endpoints detected.");
+            console.log("No internal endpoints found");
             process.exit(0);
         }
     } catch (error) {
