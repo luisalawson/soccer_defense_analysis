f2eaaeebc5cb9d1f2ea3487373813ef1df298b0f
Luisa Lawson <luisalawson14@gmail.com>
2025-01-16 11:55:09 -0300

    test1

--- .github/workflows/scripts/security-check/token2.js
+++
@@ -1,1 +0,0 @@
-const token = 'eyJhbGciOiJIUzI1NiMsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
--- .github/workflows/scripts/security-check/tricktoken.js
+++
@@ -1,1 +0,0 @@
-const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9--invalid-header-section-==.eyJ1c2VyX2lkIjoxMjM0NTY3ODkwLCJlbWFpbCI6ImZvb2BAYmFyLmNvbSJ9==..eyJ0eXBlIjoidGVzdCJ9';
