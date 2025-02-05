c12052a8f589d96ec5b605d27499b6206ad06528
Luisa Lawson <luisalawson14@gmail.com>
2025-02-05 13:19:49 -0300

    Update codeowners-check.js

--- .github/workflows/scripts/codeowners-check/codeowners-check.js
+++ .github/workflows/scripts/codeowners-check/codeowners-check.js
@@ -27,11 +27,11 @@ async function getFiles(){
     return files.data.filter(file => file.status !== "removed").map(file => file.filename);
 }
 
-async function postComment() {
+async function postComment(folderName) {
     const octokit = new Octokit({ auth: GITHUB_TOKEN });
     let commentBody = `Please update the CODEOWNERS file with the following line:\n `;
-    commentBody += `* /snap-in-name/  @${OWNER}  @devrev-ai-reviewers \n\n`;
-    commentBody += `ONLY ADD **@devrev-ai-reviewers** IF YOU ARE A MEMBER OF AAI`;
+    commentBody += `* /${folderName}/  @${OWNER}  @devrev-aai-reviewers \n\n`;
+    commentBody += `👀 ONLY ADD **@devrev-ai-reviewers** IF YOU ARE A MEMBER OF AAI ENGINEERS`;
     await octokit.request('POST /repos/{owner}/{repo}/issues/{issue_number}/comments', {
         owner: OWNER,
         repo: REPO, 
@@ -51,7 +51,14 @@ async function main() {
             process.exit(0);
         }
         const codeowners = files.find(file => file === '.github/CODEOWNERS');
-        const snapInName = files.find(file => file.includes('snap-ins'));
+        const folders = files.data.map((file) => file.filename.split("/")[0]);
+        const folderNames = [...new Set(folders)];
+        let snapInName = '';
+        if (folderNames.length > 1) {
+            snapInName = 'your-snap-in-name';
+        } else {
+            snapInName = folderNames[0];
+        }
         if (!codeowners) {
             await postComment(snapInName);
             process.exit(1);
